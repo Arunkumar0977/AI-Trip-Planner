ec83bee40ad33a2a42eda70ada5a55e190e16c45
Arun Kumar <arunkumar977977@gmail.com>
2026-04-02 10:09:09 +0530

    modification in the code

--- app/create-new-trip/page.tsx
+++ app/create-new-trip/page.tsx
@@ -1,19 +1,39 @@
 "use client";
-import React from 'react'
-import ChatBox from './_components/ChatBox'
-import Itinery from './_components/Itinery'
+import React from "react";
+import ChatBox from "./_components/ChatBox";
+import Itinery from "./_components/Itinery";
 
-export const CreateNewatrip = () => {
+export default function CreateNewTrip() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-10'>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 p-10">
       <div>
-            <ChatBox/>
+        <ChatBox />
       </div>
-      <div className='col-span-2'>
-            <Itinery/>
+      <div className="col-span-2">
+        <Itinery />
       </div>
     </div>
-  )
+  );
 }
 
-export default CreateNewatrip
+
+
+// "use client";
+// import React from 'react'
+// import ChatBox from './_components/ChatBox'
+// import Itinery from './_components/Itinery'
+
+// const CreateNewTrip = () => {
+//   return (
+//     <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-10'>
+//       <div>
+//         <ChatBox/>
+//       </div>
+//       <div className='col-span-2'>
+//         <Itinery/>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default CreateNewTrip;
